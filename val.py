1e1b3ccbaf6aa7870aaccc3b3676bc6b46148f71
dspeiser <daniel.speiser@mhp.com>
2021-12-14 15:12:36 +0100

    bugfix

--- val.py
+++ val.py
@@ -109,7 +109,7 @@ def run(data,
         callbacks=Callbacks(),
         compute_loss=None,
 #added
-        plot_dropout=False
+        plot_dropout=False, # plot most interessting dropout images
 #########
         ):
     # Initialize/load model and set device
@@ -241,7 +241,7 @@ def run(data,
             Thread(target=plot_images, args=(im, output_to_target(out), paths, f, names), daemon=True).start()
 
 #added
-        if plot_dropout:
+        if plot_dropout and batch_i < 3:
             f = save_dir / f'dropout_batch{batch_i}.jpg'  # labels
             Thread(target=plot_images, args=(im, targets, paths, f, names), daemon=True).start()
             callbacks.run('on_dropout_plot', pred, predn, path, names, im[si])
