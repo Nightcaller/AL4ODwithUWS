c1c0e46b0c3b5696999ca3fda8a7fd7c3a6e8f37
dspeiser <daniel.speiser@mhp.com>
2022-06-17 23:22:19 +0200

    bugfix

--- utils/oracle.py
+++ utils/oracle.py
@@ -169,7 +169,6 @@ def autOracle(gtPath, savePath= None,  acqPath=None, predPath=None, cycle="0"):
             hits = torch.zeros(len(gtLabels[i]))
         else:
             hits, indices = compare(gtLabels[i], predLabels[i], name)
-            print(hits)
             if acqType[i] == "ssl" and max(hits) >= pseudo_label_threshold:
                 gtLabels[i] = create_pseudo_label(hits, indices,gtLabels[i], predLabels[i], pseudo_label_threshold)
                 save_pseudo_label(gtPath, name, gtLabels[i])
