b63a145ccea197a9d21adfdf87997dff024f5c60
dspeiser <daniel.speiser@mhp.com>
2022-06-17 23:53:35 +0200

    bugfix

--- utils/oracle.py
+++ utils/oracle.py
@@ -126,6 +126,7 @@ def create_pseudo_label(hits,indices, gt, pred, threshold):
 
 def save_pseudo_label(gtPath, fileName, gtLabels):
     gtLabels = xyxy2xywh(gtLabels[:-1])
+    print(gtPath + "/" + fileName + 'NEW.txt')
     with open(gtPath + "/" + fileName + '.txt', 'a') as f:
 
         for label in gtLabels:
